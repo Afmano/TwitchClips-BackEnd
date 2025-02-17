d1a5459f2bd362146cb607460b66ad28dff6dca4
Afmano <alpritor@outlook.com>
2025-02-17 16:00:29 +0200

    GetGame and GameController changes.

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TwitchClips.InternalLogic.Twitch;
 using TwitchLib.Api;
 using TwitchLib.Api.Helix.Models.Games;
 
@@ -7,10 +8,16 @@ namespace TwitchClips.Controllers
     [ApiController, Route("api/[controller]")]
     public class GameController(TwitchAPI twitchAPI) : ControllerBase
     {
+        private readonly GameGetter _gameGetter = new(twitchAPI);
+
+        [HttpGet]
+        public async Task<ActionResult<List<Game>>> Get(int count = 20) =>
+            Ok(await _gameGetter.GetTopGames(count));
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Game>> Get(string id)
         {
-            Game? resultGame = (await twitchAPI.Helix.Games.GetGamesAsync([id])).Games.FirstOrDefault();
+            Game? resultGame = await _gameGetter.GetGame(id);
             if (resultGame == null)
             {
                 return NotFound();
--- InternalLogic/Twitch/GameGetter.cs
+++ InternalLogic/Twitch/GameGetter.cs
@@ -5,7 +5,16 @@ namespace TwitchClips.InternalLogic.Twitch
 {
     public class GameGetter(TwitchAPI twitchAPI)
     {
-        public async Task<List<Game>> GetTopGames(int gamesLimit = RequestValues.TotalLimit, int perRequest = RequestValues.TotalPerRequest)
+        public async Task<Game?> GetGame(string id) =>
+            (await twitchAPI.Helix.Games.GetGamesAsync([id])).Games.FirstOrDefault();
+
+        public async Task<List<Game>> GetTopGames(int count = 100)
+        {
+            var topGamesResponse = await twitchAPI.Helix.Games.GetTopGamesAsync(first: count);
+            return [.. topGamesResponse.Data];
+        }
+
+        public async Task<List<Game>> GetMaxTopGames(int gamesLimit = RequestValues.TotalLimit, int perRequest = RequestValues.TotalPerRequest)
         {
             List<Game> topGames = [];
             string? currentPaginationCursor = null;
